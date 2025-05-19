f3168c9f7c0b8ec03e7b594e62b809b2531fa7c9
Vanja Komadinovic <vanjakom@gmail.com>
2025-05-19 19:25:23 +0200

    0.1.0 using release version of dependencies, setting deploy

--- project.clj
+++ project.clj
@@ -1,7 +1,12 @@
-(defproject vanjakom/clj-analyzer "0.1.0-SNAPSHOT"
+(defproject vanjakom/clj-analyzer
+  "0.1.0"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :deploy-repositories [
+                        ["clojars" {
+                                    :url "https://clojars.org/repo"
+                                    :sign-releases false}]]    
   :dependencies [
-                 [org.clojure/clojure "1.11.1"]
-                 [org.clojure/tools.reader "1.5.0"]
-                 [com.mungolab/clj-common "0.3.0-SNAPSHOT"]])
+                 [com.mungolab/clj-common "0.3.2"]
+                 
+                 [org.clojure/tools.reader "1.5.0"]])
